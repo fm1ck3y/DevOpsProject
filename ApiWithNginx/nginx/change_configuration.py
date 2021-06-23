56d04b658b643c08d4f12bfa3a430153e43ac6eb
fm1ck3y <arte.vdovin@gmail.com>
2021-06-23 03:08:34 +0300

    fix change configuration

--- ApiWithNginx/nginx/change_configuration.py
+++ ApiWithNginx/nginx/change_configuration.py
@@ -5,8 +5,8 @@ if len(sys.argv) < 3:
     print("Invalid count arguments.")
     exit(0)
 
-PATH_TO_TEMPLATE_NGINX_CONF = os.getenv('PATH_TO_TEMPLATE_NGINX_CONF')
-PATH_TO_NGINX_CONF_LOCAL = os.getenv('PATH_TO_NGINX_CONF_LOCAL')
+PATH_TO_TEMPLATE_NGINX_CONF = "/opt/ApiWithNginx/nginx/nginx.conf.template"
+PATH_TO_NGINX_CONF_LOCAL = "/opt/ApiWithNginx/nginx/nginx.conf"
 
 with open(PATH_TO_TEMPLATE_NGINX_CONF,'r') as f:
     text_file = f.read()
@@ -17,5 +17,5 @@ text_file = text_file.replace(
     '${WEIGHT_API_WITH_JSON}', sys.argv[2]
 )
 
-with open(PATH_TO_TEMPLATE_NGINX_CONF,'w') as f:
+with open(PATH_TO_NGINX_CONF_LOCAL,'w') as f:
     f.write(text_file)

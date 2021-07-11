5d778a22aaec1da8bbe152314cdf614445fdef51
fm1ck3y <arte.vdovin@gmail.com>
2021-07-11 22:25:22 +0300

    add http to host and change stop on restart

--- FilesForJobs/generate_data.py
+++ FilesForJobs/generate_data.py
@@ -8,6 +8,9 @@ if len(sys.argv) < 3:
 N = int(sys.argv[1])
 HOST = sys.argv[2]
 
+if 'http://' not in HOST or 'https://' not in HOST:
+    HOST = 'http://' + HOST
+
 NAMES = ["Artem Vdovin", "Igor Lepeyko"]
 EMAILS = ["123@mail.ru","456@mail.ru"]
 USERNAMES = ["llll123", "mmmm3333"]

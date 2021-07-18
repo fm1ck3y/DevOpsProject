f2c8e26e0dcb875ebf6b98c894ec9ddf7857c9ca
fm1ck3y <arte.vdovin@gmail.com>
2021-07-18 20:46:18 +0300

    add output link to jenkins

--- start.sh
+++ start.sh
@@ -4,3 +4,4 @@ export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 terraform init
 terraform apply -auto-approve -lock=False
+echo "Jenkins = http://$(terraform output -raw jenkins_vm_ip):8080"
